df05dacbe8957575fb62318a0656e7b58073bde3
alexandr <grandmasteralexandr@gmail.com>
2019-11-18 14:46:12 +0200

    #3 add dynamic day for activities

--- src/components/ActivityBlock.vue
+++ src/components/ActivityBlock.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="activity">
-    <div class="content__date">TODAY</div>
-    <div v-for="(activity, index) in activityItems"
+    <div v-for="(activityForDay, index) in activities"
          :key="index"
-         class="activity__item"
+         class="activity__for-day"
     >
-      <div :class="activity.iconClass"></div>
-      <div class="activity__item-wrap">
-        <p class="activity__description">
-          {{activity.description}}
-        </p>
-        <p v-if="activity.comment" class="activity__comment">
-          {{activity.comment}}
-        </p>
-        <div v-if="activity.imgs" class="activity__img">
-          <ul class="activity__img-box">
-            <li v-for="(img, index) in activity.imgs"
-                :key="index"
-                class="activity__img-item"
-            >
-              <img :src="getImgUrl(img)"
-                   @click="$emit('activityImgClick', index)"
-                   alt="Activity Img"
+      <div class="content__date">{{activityForDay.activityDate}}</div>
+      <div v-for="(activity, index) in activityForDay.activityItems"
+           :key="index"
+           class="activity__item"
+      >
+        <div :class="activity.iconClass"></div>
+        <div class="activity__item-wrap">
+          <p class="activity__description">
+            {{activity.description}}
+          </p>
+          <p v-if="activity.comment" class="activity__comment">
+            {{activity.comment}}
+          </p>
+          <div v-if="activity.imgs" class="activity__img">
+            <ul class="activity__img-box">
+              <li v-for="(img, index) in activity.imgs"
+                  :key="index"
+                  class="activity__img-item"
               >
-            </li>
-          </ul>
+                <img :src="getImgUrl(img)"
+                     @click="$emit('activityImgClick', index)"
+                     alt="Activity Img"
+                >
+              </li>
+            </ul>
+          </div>
         </div>
+        <time :datetime="activity.datetime" class="activity__time">{{activity.time}}</time>
       </div>
-      <time :datetime="activity.datetime" class="activity__time">{{activity.time}}</time>
     </div>
   </div>
 </template>
@@ -38,32 +43,37 @@ export default {
 
   data() {
     return {
-      activityItems: [
+      activities: [
         {
-          iconClass: 'done-icon',
-          description: 'Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users',
-          datetime: '20:40',
-          time: '8:40 PM'
-        },
+          activityDate: 'TODAY',
+          activityItems: [
+            {
+              iconClass: 'done-icon',
+              description: 'Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users',
+              datetime: '20:40',
+              time: '8:40 PM'
+            },
 
-        {
-          iconClass: 'comment-icon',
-          description: 'Emilee Simchenko commented on Account for teams and personal in bottom style',
-          datetime: '19:32',
-          time: '7:32 PM',
-          comment: 'During a project build, it is necessary to evaluate the product design and development against project requirements and outcomes'
-        },
+            {
+              iconClass: 'comment-icon',
+              description: 'Emilee Simchenko commented on Account for teams and personal in bottom style',
+              datetime: '19:32',
+              time: '7:32 PM',
+              comment: 'During a project build, it is necessary to evaluate the product design and development against project requirements and outcomes'
+            },
 
-        {
-          iconClass: 'download-icon',
-          description: 'Darika Samak uploaded 4 files on An option to search in current projects or in all projects',
-          datetime: '18:02',
-          time: '6:02 PM',
-          imgs: [
-            'cat.jpeg',
-            'cat2.jpeg',
-            'cat3.jpeg',
-            'cat4.jpeg',
+            {
+              iconClass: 'download-icon',
+              description: 'Darika Samak uploaded 4 files on An option to search in current projects or in all projects',
+              datetime: '18:02',
+              time: '6:02 PM',
+              imgs: [
+                'cat.jpeg',
+                'cat2.jpeg',
+                'cat3.jpeg',
+                'cat4.jpeg',
+              ]
+            }
           ]
         },
       ]
