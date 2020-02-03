161d385a8106072f70edac1b04d7b999e94ffec5
Alexandr <grandmasteralexandr@gmail.com>
2020-02-03 14:41:27 +0200

    #12 add style to file block

--- src/components/content/FilesBlock.vue
+++ src/components/content/FilesBlock.vue
@@ -1,13 +1,25 @@
 <template lang="pug">
   .content-container
-    .files(v-for="file in files")
-      .file {{file}}
+    .header Files count: {{files.length}}
+    ul.files
+      li.file(
+        v-for="(file, index) in files"
+        :key="index"
+      )
+        img.preview(
+          :src="getImgUrl(file)"
+          :alt="file"
+        )
+        a.link(
+          :href="getImgUrl(file)"
+          download
+        ) {{file}}
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import {Component, Mixins} from 'vue-property-decorator'
 import {namespace} from 'vuex-class'
+import CommonMixin from '@/components/mixin/CommonMixin'
 
 const activityStore = namespace('activity');
 
@@ -16,14 +28,36 @@ const activityStore = namespace('activity');
     name: "FilesBlock"
   }
 )
-export default class FilesBlock extends Vue {
+export default class FilesBlock extends Mixins(CommonMixin) {
   @activityStore.Getter('files') files!: Function;
 }
 </script>
 
 <style scoped lang="scss">
+  .header {
+    align-self: center;
+    font-weight: bold;
+    font-size: 18px;
+  }
+
   .files {
+    padding: 0;
+  }
+
+  .file {
+    list-style-type: none;
     display: flex;
-    justify-content: center;
+    align-items: center;
+    margin: 10px 0;
+  }
+
+  .preview {
+    @include square-block(30px);
+    object-fit: cover;
+    border-radius: 2px;
+  }
+
+  .link {
+    margin-left: 5px;
   }
 </style>
